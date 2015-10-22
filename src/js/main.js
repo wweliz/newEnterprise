a23112d59f1ca9b3b1800e18e1223e8073139bef
Whitney Williams <wweliz@gmail.com>
2015-10-22 02:24:42 -0400

    functions to show/hide the pricing details panel

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,12 @@ var $ = require('jquery');
 var foundation = require('foundation');
 
 $(document).foundation();
+
+// Show/hide the pricing details panel ///////////////////////////////////
+$('.pricing-details').hide();
+
+$('.toggle-panel-tools').click(function(e) {
+  $(this).find('.fa').toggleClass('fa-rotate-45');
+  $(this).parent().next().slideToggle(200);
+  e.preventDefault();
+});
